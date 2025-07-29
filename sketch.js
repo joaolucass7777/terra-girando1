b8ad61b46365293442368af7085e278f31ede512
joaolucass7777 <joao.cesar.proenca@escola.pr.gov.br>
2025-07-29 11:56:07 -0300

    ggg

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ function setup() {
 }
 
 function draw() {
-  background(205, 102, 94); //cor de fundo
+  background(245, 172, 67); //cor de fundo
   rotateY(millis()/1000); // rotação da terra
   texture(terra); //textura da terra, imagem.
-  sphere(80, 100); //esfera 3D
+  sphere(250, 500); //esfera 3D
   
   
 }
